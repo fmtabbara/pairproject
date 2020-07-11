04b8c86274f51926f76b82fe3af4beb4ab14eaa3
kev2030 <62866525+kev2030@users.noreply.github.com>
2020-07-11 13:40:51 -0600

    please check my code

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,45 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+  const initialState = [
+    {
+      name: 'todo1',
+      description: 'coding',
+      complete: false,
+    },
+    {
+      name: 'todo2',
+      description: 'reading',
+      complete: false,
+    },
+    {
+      name: 'todo3',
+      description: 'jogging',
+      complete: false,
+    },
+  ]
+
+  const [todos, setTodos] = useState(initialState)
+  const [newTodo, setNewTodo] = useState('')
+
+  const addNewTodo = () => {
+    const newArray = [...todos, { name: newTodo }]
+    setTodos(newArray)
+    setNewTodo('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todos.map((todo) => (
+        <div key={todos.id}>
+          {todos.name}
+          {todos.description}
+          {todos.complete}
+        </div>
+      ))}
+      <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+      <button onClick={addNewTodo}>Add new todo</button>
     </div>
   )
 }
