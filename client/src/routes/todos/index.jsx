2b100f6157f7b575989321ca580fb75ae76d763d
kev2030 <62866525+kev2030@users.noreply.github.com>
2020-07-25 16:06:56 -0600

    use new todo variable

--- client/src/routes/todos/index.jsx
+++ client/src/routes/todos/index.jsx
@@ -1,45 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Todo } from '../../components/Todo/Todo'
 import { useContext } from 'react'
 import { AuthContext } from '../../global/auth/context'
 import { Link } from 'react-router-dom'
 import { Loading } from '../../components/loading/loading'
 import { Page } from '../../components/page'
-
-const initialState = [
-  {
-    name: 'todo1',
-    description: 'coding',
-    complete: false,
-  },
-  {
-    name: 'todo2',
-    description: 'reading',
-    complete: false,
-  },
-  {
-    name: 'todo3',
-    description: 'jogging',
-    complete: false,
-  },
-]
+import { useFetch } from '../../hooks/useFetch'
 
 export const Todos = () => {
   const { token } = useContext(AuthContext)
-  const [loading, setLoading] = useState(true)
-  const [todos, setTodos] = useState(initialState)
+  const [todos, setTodos] = useState([])
   const [newTodo, setNewTodo] = useState('')
 
   const addNewTodo = () => {
-    const newArray = [
-      ...todos,
-      { description: newTodo, name: newTodo, complete: false },
-    ]
-    setTodos(newArray)
+    fetch('/todos/ali@gmail.com', {
+      method: 'POST',
+      body: JSON.stringify({ name: newTodo }),
+    })
     setNewTodo('')
   }
 
-  setTimeout(() => setLoading(false), 1000)
+  const { results = [], error, loading, fetch } = useFetch()
+  useEffect(() => {
+    setTodos([...results])
+  }, [results])
+
+  useEffect(() => {
+    fetch('/todos/ali@gmail.com')
+  }, [])
 
   return (
     <Page>
@@ -51,7 +39,7 @@ export const Todos = () => {
             <Todo
               key={index}
               name={todo.name}
-              description={todo.description}
+              description={todo.name}
               complete={false}
             />
           ))}
